c0399b2553c1d8f92d1e912400f77112a4dea9ee
rezky <rezky.setiawan85@gmail.com>
2020-11-25 06:49:26 +0700

    add chat api routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => '/chat'], function () {
+    Route::get('/','Api\ChatController@index');
+    Route::post('/','Api\ChatController@store');
+});
