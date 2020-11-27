577b21d2872521d9981d63cd2a6738955b498a05
rezky setiawan <rezky.setiawan85@gmail.com>
2020-11-27 15:26:22 +0700

    change to cdn bootstrap

--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -5,8 +5,7 @@
     <title>@yield('page_name') | Cup Chatbot</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
-    <script type="text/javascript" src="{{ asset('js/jquery-3.5.1.min.js') }}"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <style>
         body {
             font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
